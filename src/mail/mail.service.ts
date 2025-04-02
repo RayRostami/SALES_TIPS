8ddb9557402d61ea0a9e2355913be6a3d4030e8f
Ray Rostami <rostami_r@yahoo.com>
2025-04-02 13:48:06 -0400

    email

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -14,15 +14,14 @@ export class MailService {
     this.transporter = nodemailer.createTransport({
       host: this.configService.get<string>('EMAIL_HOST'),
       port: this.configService.get<number>('EMAIL_PORT'),
-      secure: this.configService.get<boolean>('EMAIL_SECURE'),
+      secure: false,
       auth: {
         user: this.configService.get<string>('EMAIL_USER'),
         pass: this.configService.get<string>('EMAIL_PASSWORD'),
       },
-      // tls: {
-      //   minVersion: 'TLSv1.2',
-      //   rejectUnauthorized: true
-      // }
+      tls: {
+        rejectUnauthorized: false, // optional, avoid cert issues
+      },
     });
   }
   async sendTesting(to:any, from:any, subject:any, text:any){
